fd9900112bea784c4bedea97a84ce567b5c0ee8a
Yagiz Hitit <yagizhitit@gmail.com>
2024-06-22 05:48:57 +0100

    HomeScreen

---
+++ components/Trending.jsx
@@ -0,0 +1,18 @@
+import { View, Text } from "react-native";
+import React from "react";
+import { FlatList } from "react-native";
+
+const Trending = ({ posts }) => {
+  return (
+    <FlatList
+      data={posts}
+      keyExtractor={(item) => item.$id}
+      renderItem={({ item }) => (
+        <Text className="text-3xl text-white">{item.id}</Text>
+      )}
+      horizontal
+    />
+  );
+};
+
+export default Trending;
